574fcfb97a7483345f418504249b68a6ad155e82
DamyGenius <damian.meza.py@hotmail.com>
2016-07-25 17:49:20 -0400

    Mejoras en Seguridad

--- src/main/java/com/fpuna/py/travelware/dao/UsuarioDao.java
+++ src/main/java/com/fpuna/py/travelware/dao/UsuarioDao.java
@@ -8,12 +8,14 @@ package com.fpuna.py.travelware.dao;
 import com.fpuna.py.travelware.model.PgeUsuarios;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 /**
  *
  * @author eencina
  */
 public interface UsuarioDao extends GenericDao<PgeUsuarios, Integer>{
+    public List<PgeUsuarios> getAllTot();
     public PgeUsuarios autenticate(String username, String password) throws UnsupportedEncodingException, NoSuchAlgorithmException;
     public PgeUsuarios changePass(String username, String newPassword);
     public PgeUsuarios getByName(String name);
